c08d09c66bf7319166aa340a989c1be2e855f66f
BridgetteBXP13 <62217158+BridgetteBXP13@users.noreply.github.com>
2022-09-30 14:25:37 -0500

    Updating

---
+++ ML Algorithms from Scratch/LinearRegression.cpp
@@ -0,0 +1,130 @@
+// Machine Learning Scratch.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int getNumLines(ifstream);						// Counts and returns the number of lines in a given file stream
+double calcAccuracy(int, int, int, int);		// Calculates and returns the accuracy from the given TP, TN, FP, and FN
+double calcSensitivity(int, int);				// Calculates and returns the sensitivity from the given TP and FN
+double calcSpecificity(int, int);				// Calculates and returns the specificity from the given TN and 
+
+void logisticRegression();		// Performs logistic regression on the given train and test sets
+void naiveBayes();
+
+
+int main()
+{
+	ifstream inFS;
+	string line;
+	string person_in, pclass_in, survived_in, sex_in, age_in;
+	const int MAX_LEN = 10000;
+	vector<double> personIDTrain(MAX_LEN);
+	vector<double> pclassTrain(MAX_LEN);
+	vector<double> survivedTrain(MAX_LEN);
+	vector<double> sexTrain(MAX_LEN);
+	vector<double> ageTrain(MAX_LEN);
+	vector<double> personIDTest(MAX_LEN);
+	vector<double> pclassTest(MAX_LEN);
+	vector<double> survivedTest(MAX_LEN);
+	vector<double> sexTest(MAX_LEN);
+	vector<double> ageTest(MAX_LEN);
+	vector<double> numOneTrain(MAX_LEN);
+	vector<double> numOneTest(MAX_LEN);
+	int numEntry = 0;
+	int numEntry2 = 0;
+
+	// Opening the titanic project data file
+	cout << "Opening file titanic_project.csv" << endl << endl;
+
+	inFS.open("titanic_project.csv");
+
+	// If we couldn't open the file
+	if (!inFS.is_open())
+	{
+		cout << "Couldn't open the file :(" << endl;
+	}	// File successfully opened
+	else
+	{
+		//cout << "Reading line 1" << endl;
+		getline(inFS, line);
+
+		//cout << "heading: " << line << endl;
+
+		// While there is more to read from the file
+		while (inFS.good())
+		{
+			getline(inFS, person_in, ',');
+			getline(inFS, pclass_in, ',');
+			getline(inFS, survived_in, ',');
+			getline(inFS, sex_in, ',');
+			getline(inFS, age_in, '\n');
+
+			if (numEntry < 1046) {
+				if (numEntry < 800) {
+					personIDTrain.at(numEntry) = stod(person_in);
+					pclassTrain.at(numEntry) = stod(pclass_in);
+					survivedTrain.at(numEntry) = stod(survived_in);
+					sexTrain.at(numEntry) = stod(sex_in);
+					ageTrain.at(numEntry) = stod(age_in);
+					numOneTrain.at(numEntry) = 1;
+				}
+				else {
+					personIDTest.at(numEntry2) = stod(person_in);
+					pclassTest.at(numEntry2) = stod(pclass_in);
+					survivedTest.at(numEntry2) = stod(survived_in);
+					sexTest.at(numEntry2) = stod(sex_in);
+					ageTest.at(numEntry2) = stod(age_in);
+					cout << "numEntry 2: " << numEntry2 << endl;
+					numEntry2++;
+				}
+
+			}
+
+			numEntry++;
+		}
+		
+		personIDTrain.resize(800);
+		personIDTest.resize((numEntry - 800));
+		pclassTest.resize((numEntry - 800));
+		pclassTrain.resize(800);
+		survivedTest.resize((numEntry - 800));
+		survivedTrain.resize(800);
+		sexTest.resize((numEntry - 800));
+		sexTrain.resize(800);
+		ageTest.resize((numEntry - 800));
+		ageTrain.resize(800);
+		
+		vector <double> matrixTrainSex[800][2];
+
+
+		cout << "Number of Rows: " << numEntry << endl;
+		inFS.close();
+
+	}
+}
+
+// Counts and returns the number of lines in a given file stream
+int getNumLines(ifstream file)
+{
+	int numOfLines = 0;		// The number of lines
+	string line;			// The current line of the file read (won't be used)
+	// While we can read lines from the file, count it as a line
+	while(getline(file, line)) {
+		numOfLines++;	// Add one to the number of lines
+	}
+
+	// Return the number of lines found
+	return numOfLines;
+}
+double calcAccuracy(int, int, int, int);		// Calculates and returns the accuracy from the given TP, TN, FP, and FN
+double calcSensitivity(int, int);				// Calculates and returns the sensitivity from the given TP and FN
+double calcSpecificity(int, int);				// Calculates and returns the specificity from the given TN and 
+
+void logisticRegression();		// Performs logistic regression on the given train and test sets
+void naiveBayes();
